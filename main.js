d5fe6ab54fb2ebd8ffa347ff0ce1e0e0bc8119a9
Fábio Maia <fabiu.maia@gmail.com>
2017-10-18 16:57:11 +0100

    Add wip site

--- main.js
+++ main.js
@@ -61,7 +61,7 @@ function createProgram(gl, vertexShader, fragmentShader) {
 
 function main() {
   // Get A WebGL context
-  var canvas = document.getElementById("c");
+  var canvas = document.getElementById("canvas");
   var gl = canvas.getContext("webgl2");
   if (!gl) {
     return;
@@ -105,8 +105,7 @@ function main() {
   var normalize = false; // don't normalize the data
   var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
   var offset = 0;        // start at the beginning of the buffer
-  gl.vertexAttribPointer(
-      positionAttributeLocation, size, type, normalize, stride, offset);
+  gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset);
 
   webglUtils.resizeCanvasToDisplaySize(gl.canvas);
 
@@ -114,7 +113,7 @@ function main() {
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
   // Clear the canvas
-  gl.clearColor(0, 0, 0, 0);
+  gl.clearColor(1, 1, 0, 0.5);
   gl.clear(gl.COLOR_BUFFER_BIT);
 
   // Tell it to use our program (pair of shaders)

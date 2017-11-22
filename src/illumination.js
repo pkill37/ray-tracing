9be2f024c57b101671155acdb800b98248470b45
Fábio Maia <fabiu.maia@gmail.com>
2017-11-22 11:43:26 +0000

    Refactor

---
+++ src/illumination.js
@@ -0,0 +1,184 @@
+// Ambient coef.
+var kAmbi = [ 0.2, 0.2, 0.2 ];
+
+// Difuse coef.
+var kDiff = [ 0.7, 0.7, 0.7 ];
+
+// Specular coef.
+var kSpec = [ 0.7, 0.7, 0.7 ];
+
+// Phong coef.
+var nPhong = 100;
+
+//  Computing the illumination and rendering the model
+
+function computeIllumination( mvMatrix ) {
+
+	// Phong Illumination Model
+	
+	// Clearing the colors array
+	
+	for( var i = 0; i < colors.length; i++ )
+	{
+		colors[i] = 0.0;
+	}
+	
+    // SMOOTH-SHADING 
+
+    // Compute the illumination for every vertex
+
+    // Iterate through the vertices
+    
+    for( var vertIndex = 0; vertIndex < vertices.length; vertIndex += 3 )
+    {	
+		// For every vertex
+		
+		// GET COORDINATES AND NORMAL VECTOR
+		
+		var auxP = vertices.slice( vertIndex, vertIndex + 3 );
+		
+		var auxN = normals.slice( vertIndex, vertIndex + 3 );
+
+        // CONVERT TO HOMOGENEOUS COORDINATES
+
+		auxP.push( 1.0 );
+		
+		auxN.push( 0.0 );
+		
+        // APPLY CURRENT TRANSFORMATION
+
+        var pointP = multiplyPointByMatrix( mvMatrix, auxP );
+
+        var vectorN = multiplyVectorByMatrix( mvMatrix, auxN );
+        
+        normalize( vectorN );
+
+		// VIEWER POSITION
+		
+		var vectorV = vec3();
+		
+		if( projectionType == 0 ) {
+		
+			// Orthogonal 
+			
+			vectorV[2] = 1.0;
+		}	
+		else {
+			
+		    // Perspective
+		    
+		    // Viewer at ( 0, 0 , 0 )
+		
+			vectorV = symmetric( pointP );
+		}
+		
+        normalize( vectorV );
+
+	    // Compute the 3 components: AMBIENT, DIFFUSE and SPECULAR
+	    
+	    // FOR EACH LIGHT SOURCE
+	    
+	    for(var l = 0; l < lightSources.length; l++ )
+	    {
+			if( lightSources[l].isOff() ) {
+				
+				continue;
+			}
+			
+	        // INITIALIZE EACH COMPONENT, with the constant terms
+	
+		    var ambientTerm = vec3();
+		
+		    var diffuseTerm = vec3();
+		
+		    var specularTerm = vec3();
+		
+		    // For the current light source
+		
+		    ambient_Illumination = lightSources[l].getAmbIntensity();
+		
+		    int_Light_Source = lightSources[l].getIntensity();
+		
+		    pos_Light_Source = lightSources[l].getPosition();
+		    
+		    // Animating the light source, if defined
+		    
+		    var lightSourceMatrix = mat4();
+		    
+		    // COMPLETE THE CODE FOR THE OTHER ROTATION AXES
+		    
+		    if( lightSources[l].isRotYYOn() ) 
+		    {
+				lightSourceMatrix = mult( 
+						lightSourceMatrix, 
+						rotationYYMatrix( lightSources[l].getRotAngleYY() ) );
+			}
+			
+	        for( var i = 0; i < 3; i++ )
+	        {
+			    // AMBIENT ILLUMINATION --- Constant for every vertex
+	   
+			    ambientTerm[i] = ambient_Illumination[i] * kAmbi[i];
+	
+	            diffuseTerm[i] = int_Light_Source[i] * kDiff[i];
+	
+	            specularTerm[i] = int_Light_Source[i] * kSpec[i];
+	        }
+	    
+	        // DIFFUSE ILLUMINATION
+	        
+	        var vectorL = vec4();
+	
+	        if (pos_Light_Source[3] == 0.0) {
+	            // DIRECTIONAL Light Source
+	            
+	            vectorL = multiplyVectorByMatrix( 
+							lightSourceMatrix,
+							pos_Light_Source );
+	        } else {
+	            // POINT Light Source
+	            // TODO : apply the global transformation to the light source?	
+	            vectorL = multiplyPointByMatrix(lightSourceMatrix, pos_Light_Source);
+	
+				for(var i = 0; i < 3; i++) {
+	                vectorL[i] -= pointP[i];
+	            }
+	        }
+	
+			// Back to Euclidean coordinates
+			
+			vectorL = vectorL.slice(0, 3);
+			
+	        normalize(vectorL);
+	
+	        var cosNL = dotProduct(vectorN, vectorL);
+	
+            // No direct illumination !!
+	        if(cosNL < 0.0) cosNL = 0.0;
+	
+	        // SEPCULAR ILLUMINATION 
+	
+	        var vectorH = add(vectorL, vectorV);
+	        normalize(vectorH);
+	        var cosNH = dotProduct(vectorN, vectorH);
+	
+			// No direct illumination or viewer not in the right direction
+	        if((cosNH < 0.0) || (cosNL <= 0.0)) cosNH = 0.0;
+	
+	        // Compute the color values and store in the colors array
+	        var tempR = ambientTerm[0] + diffuseTerm[0] * cosNL + specularTerm[0] * Math.pow(cosNH, nPhong);
+	        var tempG = ambientTerm[1] + diffuseTerm[1] * cosNL + specularTerm[1] * Math.pow(cosNH, nPhong);
+	        var tempB = ambientTerm[2] + diffuseTerm[2] * cosNL + specularTerm[2] * Math.pow(cosNH, nPhong);
+	        
+			colors[vertIndex] += tempR;
+			colors[vertIndex] = Math.min(colors[vertIndex], 1.0);
+	        
+			colors[vertIndex + 1] += tempG;
+			colors[vertIndex + 1] = Math.min(colors[vertIndex + 1], 1.0);
+			
+			colors[vertIndex + 2] += tempB;
+			colors[vertIndex + 2] = Math.min(colors[vertIndex + 2], 1.0);
+	    }	
+	}
+}
+
--- src/main.js
+++ src/main.js
@@ -45,32 +45,12 @@ var rotationYY_SPEED = 1;
 var rotationZZ_ON = 1;
 var rotationZZ_DIR = 1;
 var rotationZZ_SPEED = 1;
- 
+
 // To allow choosing the way of drawing the model triangles
-
 var primitiveType = null;
- 
+
 // To allow choosing the projection type
-
 var projectionType = 0;
-
-// NEW --- Model Material Features
-
-// Ambient coef.
-
-var kAmbi = [ 0.2, 0.2, 0.2 ];
-
-// Difuse coef.
-
-var kDiff = [ 0.7, 0.7, 0.7 ];
-
-// Specular coef.
-
-var kSpec = [ 0.7, 0.7, 0.7 ];
-
-// Phong coef.
-
-var nPhong = 100;
 
 // Initial model has just ONE TRIANGLE
 
@@ -99,226 +79,37 @@ var colors = [
 		 1.00,  0.00,  0.00,
 ];
 
-//----------------------------------------------------------------------------
-//
-// The WebGL code
-//
-
-//----------------------------------------------------------------------------
-//
-//  Rendering
-//
-
 // Handling the Vertex and the Color Buffers
 
-function initBuffers() {	
-	
+function initBuffers() {
 	// Coordinates
-		
 	triangleVertexPositionBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 	triangleVertexPositionBuffer.itemSize = 3;
-	triangleVertexPositionBuffer.numItems = vertices.length / 3;			
+	triangleVertexPositionBuffer.numItems = vertices.length / 3;
 
 	// Associating to the vertex shader
-	
-	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, 
-			triangleVertexPositionBuffer.itemSize, 
+
+	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute,
+			triangleVertexPositionBuffer.itemSize,
 			gl.FLOAT, false, 0, 0);
-	
+
 	// Colors
-		
 	triangleVertexColorBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexColorBuffer);
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
 	triangleVertexColorBuffer.itemSize = 3;
-	triangleVertexColorBuffer.numItems = colors.length / 3;			
+	triangleVertexColorBuffer.numItems = colors.length / 3;
 
 	// Associating to the vertex shader
-	
-	gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, 
-			triangleVertexColorBuffer.itemSize, 
+
+	gl.vertexAttribPointer(shaderProgram.vertexColorAttribute,
+			triangleVertexColorBuffer.itemSize,
 			gl.FLOAT, false, 0, 0);
-
-
 }
 
-//----------------------------------------------------------------------------
-
-//  Computing the illumination and rendering the model
-
-function computeIllumination( mvMatrix ) {
-
-	// Phong Illumination Model
-	
-	// Clearing the colors array
-	
-	for( var i = 0; i < colors.length; i++ )
-	{
-		colors[i] = 0.0;
-	}
-	
-    // SMOOTH-SHADING 
-
-    // Compute the illumination for every vertex
-
-    // Iterate through the vertices
-    
-    for( var vertIndex = 0; vertIndex < vertices.length; vertIndex += 3 )
-    {	
-		// For every vertex
-		
-		// GET COORDINATES AND NORMAL VECTOR
-		
-		var auxP = vertices.slice( vertIndex, vertIndex + 3 );
-		
-		var auxN = normals.slice( vertIndex, vertIndex + 3 );
-
-        // CONVERT TO HOMOGENEOUS COORDINATES
-
-		auxP.push( 1.0 );
-		
-		auxN.push( 0.0 );
-		
-        // APPLY CURRENT TRANSFORMATION
-
-        var pointP = multiplyPointByMatrix( mvMatrix, auxP );
-
-        var vectorN = multiplyVectorByMatrix( mvMatrix, auxN );
-        
-        normalize( vectorN );
-
-		// VIEWER POSITION
-		
-		var vectorV = vec3();
-		
-		if( projectionType == 0 ) {
-		
-			// Orthogonal 
-			
-			vectorV[2] = 1.0;
-		}	
-		else {
-			
-		    // Perspective
-		    
-		    // Viewer at ( 0, 0 , 0 )
-		
-			vectorV = symmetric( pointP );
-		}
-		
-        normalize( vectorV );
-
-	    // Compute the 3 components: AMBIENT, DIFFUSE and SPECULAR
-	    
-	    // FOR EACH LIGHT SOURCE
-	    
-	    for(var l = 0; l < lightSources.length; l++ )
-	    {
-			if( lightSources[l].isOff() ) {
-				
-				continue;
-			}
-			
-	        // INITIALIZE EACH COMPONENT, with the constant terms
-	
-		    var ambientTerm = vec3();
-		
-		    var diffuseTerm = vec3();
-		
-		    var specularTerm = vec3();
-		
-		    // For the current light source
-		
-		    ambient_Illumination = lightSources[l].getAmbIntensity();
-		
-		    int_Light_Source = lightSources[l].getIntensity();
-		
-		    pos_Light_Source = lightSources[l].getPosition();
-		    
-		    // Animating the light source, if defined
-		    
-		    var lightSourceMatrix = mat4();
-		    
-		    // COMPLETE THE CODE FOR THE OTHER ROTATION AXES
-		    
-		    if( lightSources[l].isRotYYOn() ) 
-		    {
-				lightSourceMatrix = mult( 
-						lightSourceMatrix, 
-						rotationYYMatrix( lightSources[l].getRotAngleYY() ) );
-			}
-			
-	        for( var i = 0; i < 3; i++ )
-	        {
-			    // AMBIENT ILLUMINATION --- Constant for every vertex
-	   
-			    ambientTerm[i] = ambient_Illumination[i] * kAmbi[i];
-	
-	            diffuseTerm[i] = int_Light_Source[i] * kDiff[i];
-	
-	            specularTerm[i] = int_Light_Source[i] * kSpec[i];
-	        }
-	    
-	        // DIFFUSE ILLUMINATION
-	        
-	        var vectorL = vec4();
-	
-	        if (pos_Light_Source[3] == 0.0) {
-	            // DIRECTIONAL Light Source
-	            
-	            vectorL = multiplyVectorByMatrix( 
-							lightSourceMatrix,
-							pos_Light_Source );
-	        } else {
-	            // POINT Light Source
-	            // TODO : apply the global transformation to the light source?	
-	            vectorL = multiplyPointByMatrix(lightSourceMatrix, pos_Light_Source);
-	
-				for(var i = 0; i < 3; i++) {
-	                vectorL[i] -= pointP[i];
-	            }
-	        }
-	
-			// Back to Euclidean coordinates
-			
-			vectorL = vectorL.slice(0, 3);
-			
-	        normalize(vectorL);
-	
-	        var cosNL = dotProduct(vectorN, vectorL);
-	
-            // No direct illumination !!
-	        if(cosNL < 0.0) cosNL = 0.0;
-	
-	        // SEPCULAR ILLUMINATION 
-	
-	        var vectorH = add(vectorL, vectorV);
-	        normalize(vectorH);
-	        var cosNH = dotProduct(vectorN, vectorH);
-	
-			// No direct illumination or viewer not in the right direction
-	        if((cosNH < 0.0) || (cosNL <= 0.0)) cosNH = 0.0;
-	
-	        // Compute the color values and store in the colors array
-	        var tempR = ambientTerm[0] + diffuseTerm[0] * cosNL + specularTerm[0] * Math.pow(cosNH, nPhong);
-	        var tempG = ambientTerm[1] + diffuseTerm[1] * cosNL + specularTerm[1] * Math.pow(cosNH, nPhong);
-	        var tempB = ambientTerm[2] + diffuseTerm[2] * cosNL + specularTerm[2] * Math.pow(cosNH, nPhong);
-	        
-			colors[vertIndex] += tempR;
-			colors[vertIndex] = Math.min(colors[vertIndex], 1.0);
-	        
-			colors[vertIndex + 1] += tempG;
-			colors[vertIndex + 1] = Math.min(colors[vertIndex + 1], 1.0);
-			
-			colors[vertIndex + 2] += tempB;
-			colors[vertIndex + 2] = Math.min(colors[vertIndex + 2], 1.0);
-	    }	
-	}
-}
-
-function drawModel( angleXX, angleYY, angleZZ, 
+function drawModel( angleXX, angleYY, angleZZ,
 					sx, sy, sz,
 					tx, ty, tz,
 					mvMatrix,
@@ -355,31 +146,24 @@ function drawModel( angleXX, angleYY, angleZZ,
 	// This can be done in a better way !!
 
 	initBuffers();
-	
-	// Drawing 
-	
+
+	// Drawing
+
 	// primitiveType allows drawing as filled triangles / wireframe / vertices
-	
+
 	if( primitiveType == gl.LINE_LOOP ) {
-		
 		// To simulate wireframe drawing!
-		
 		// No faces are defined! There are no hidden lines!
-		
 		// Taking the vertices 3 by 3 and drawing a LINE_LOOP
-		
+
 		var i;
-		
 		for( i = 0; i < triangleVertexPositionBuffer.numItems / 3; i++ ) {
-		
-			gl.drawArrays( primitiveType, 3 * i, 3 ); 
+			gl.drawArrays( primitiveType, 3 * i, 3 );
 		}
-	}	
+	}
 	else {
-				
-		gl.drawArrays(primitiveType, 0, triangleVertexPositionBuffer.numItems); 
-		
-	}	
+		gl.drawArrays(primitiveType, 0, triangleVertexPositionBuffer.numItems);
+	}
 }
 
 //----------------------------------------------------------------------------
@@ -387,73 +171,49 @@ function drawModel( angleXX, angleYY, angleZZ,
 //  Drawing the 3D scene
 
 function drawScene() {
-	
 	var pMatrix;
-	
 	var mvMatrix = mat4();
-	
-	// Clearing the frame-buffer and the depth-buffer
-	
-	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-	
-	// Computing the Projection Matrix
-	
-	if( projectionType == 0 ) {
-		
-		// For now, the default orthogonal view volume
-		
-		pMatrix = ortho( -1.0, 1.0, -1.0, 1.0, -1.0, 1.0 );
-		
-		// Global transformation !!
-		
-		globalTz = 0.0;
-		
-		// TO BE DONE !
-		
-		// Allow the user to control the size of the view volume
-	}
-	else {	
 
-		// A standard view volume.
-		
-		// Viewer is at (0,0,0)
-		
-		// Ensure that the model is "inside" the view volume
-		
-		pMatrix = perspective( 45, 1, 0.05, 15 );
-		
+	// Clearing the frame-buffer and the depth-buffer
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+	// Computing the Projection Matrix
+	if( projectionType == 0 ) {
+		// For now, the default orthogonal view volume
+		pMatrix = ortho( -1.0, 1.0, -1.0, 1.0, -1.0, 1.0 );
+
 		// Global transformation !!
-		
+		globalTz = 0.0;
+
+		// TO BE DONE !
+		// Allow the user to control the size of the view volume
+	} else {
+		// A standard view volume.
+		// Viewer is at (0,0,0)
+		// Ensure that the model is "inside" the view volume
+		pMatrix = perspective( 45, 1, 0.05, 15 );
+
+		// Global transformation !!
 		globalTz = -2.5;
 
 		// TO BE DONE !
-		
 		// Allow the user to control the size of the view volume
 	}
-	
+
 	// Passing the Projection Matrix to apply the current projection
-	
 	var pUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
-	
 	gl.uniformMatrix4fv(pUniform, false, new Float32Array(flatten(pMatrix)));
-	
+
 	// GLOBAL TRANSFORMATION FOR THE WHOLE SCENE
-	
 	mvMatrix = translationMatrix( 0, 0, globalTz );
-	
+
 	// Instantianting the current model
-		
-	drawModel( angleXX, angleYY, angleZZ, 
+	drawModel( angleXX, angleYY, angleZZ,
 	           sx, sy, sz,
 	           tx, ty, tz,
 	           mvMatrix,
 	           primitiveType );
 }
-
-//----------------------------------------------------------------------------
-//
-//  NEW --- Animation
-//
 
 // Animation --- Updating transformation parameters
 
@@ -461,18 +221,15 @@ var lastTime = 0;
 
 function animate() {
 	var timeNow = new Date().getTime();
-	
+
 	if(lastTime != 0) {
 		var elapsed = timeNow - lastTime;
-		
 		// Global rotation
-		
 		if(globalRotationYY_ON ) {
 			globalAngleYY += globalRotationYY_DIR * globalRotationYY_SPEED * (90 * elapsed) / 1000.0;
 	    }
 
 		// Local rotations
-		
 		if(rotationXX_ON) {
 			angleXX += rotationXX_DIR * rotationXX_SPEED * (90 * elapsed) / 1000.0;
 	    }
@@ -486,7 +243,6 @@ function animate() {
 	    }
 
 		// Rotating the light sources
-	
 		for(var i = 0; i < lightSources.length; i++ ) {
 			if(lightSources[i].isRotYYOn()) {
 				var angle = lightSources[i].getRotAngleYY() + lightSources[i].getRotationSpeed() * (90 * elapsed) / 1000.0;
@@ -494,7 +250,6 @@ function animate() {
 			}
 		}
 	}
-	
 	lastTime = timeNow;
 }
 
@@ -511,7 +266,7 @@ function tick() {
 function initWebGL( canvas ) {
 	try {
 		gl = canvas.getContext("webgl2");
-        
+
         var color = [Math.random(), Math.random(), Math.random(), 1];
         gl.clearColor(...color);
 

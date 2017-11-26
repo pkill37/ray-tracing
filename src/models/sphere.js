6abe26fefbc7d573bc190f06d764eed929722972
Fábio Maia <fabiu.maia@gmail.com>
2017-11-26 18:13:14 +0000

    Refactor sphere constructor

--- src/models/sphere.js
+++ src/models/sphere.js
@@ -1,5 +1,5 @@
 class Sphere extends Model {
-    constructor(center, radius, color, mvMatrix, translation, rotation, scale) {
+    constructor(center, radius, color, rotation) {
         super(sphereVertices, flatten(Array(sphereVertices.length/3).fill(color.slice(0, 3))), null, gl.TRIANGLES, center, rotation, Array(3).fill(radius));
         this.center = center
         this.radius = radius
